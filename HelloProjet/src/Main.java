311b218856a7701dc41f3a542391dbba94af0fbb
licunyang <1018896518@qq.com>
2019-08-06 21:17:08 +0800

    李四提交的代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("再次提交新的代码到服务器");
+		System.out.println("李四提交的代码到服务器");
     }
 }
